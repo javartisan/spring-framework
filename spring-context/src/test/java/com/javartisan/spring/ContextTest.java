e0c867961c3ceed95514c6c76b81a0e7486c12a8
leodaxin <leodaxin@163.com>
2019-12-22 00:10:03 +0800

    测试如何调用org.springframework.context.annotation.AnnotationConfigUtils#registerAnnotationConfigProcessors(org.springframework.beans.factory.support.BeanDefinitionRegistry, java.lang.Object)进行内部处理器注册

--- spring-context/src/test/java/com/javartisan/spring/ContextTest.java
+++ spring-context/src/test/java/com/javartisan/spring/ContextTest.java
@@ -4,15 +4,28 @@ import com.javartisan.spring.config.Config;
 import com.javartisan.spring.service.UserService;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContextTest {
 
+	private static final String PATH = "/org/springframework/context/support/";
+	private static final String RESOURCE_CONTEXT = PATH + "ClassPathXmlApplicationContextTests-resource.xml";
+
+
 	@Test
-	public void testContext() {
+	public void testAnnotationConfigApplicationContextStart() {
 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 		UserService userService = context.getBean(UserService.class);
 		System.out.println(userService);
+	}
+
+
+	@Test
+	public void testClassPathXmlApplicationContext() {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(RESOURCE_CONTEXT);
+		System.out.println(context);
 	}
 
 
