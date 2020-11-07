8195de25a6cd429f8a121c356d9546cc29498f02
daxin <leodaxin@163.com>
2020-11-07 12:10:03 +0800

    添加默认构造器

--- spring-context/src/test/java/com/javartisan/spring/repo/UserDao.java
+++ spring-context/src/test/java/com/javartisan/spring/repo/UserDao.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserDao {
+	public UserDao() {
+		System.out.println("---------");
+	}
+
 	@Override
 	public String toString() {
 		return "UserDao Repo";
