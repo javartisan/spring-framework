196496510530a76ac9c3004ac53422ac9cbf83e8
liuguangxin2 <liuguangxin@jd.com>
2020-11-07 23:25:40 +0800

    事务解析的基类

--- spring-tx/src/main/java/org/springframework/transaction/annotation/TransactionAnnotationParser.java
+++ spring-tx/src/main/java/org/springframework/transaction/annotation/TransactionAnnotationParser.java
@@ -22,6 +22,8 @@ import org.springframework.lang.Nullable;
 import org.springframework.transaction.interceptor.TransactionAttribute;
 
 /**
+ * 事务属性解析器，用于将事务配置信息封装成: {@link TransactionAttribute}
+ * <p>
  * Strategy interface for parsing known transaction annotation types.
  * {@link AnnotationTransactionAttributeSource} delegates to such
  * parsers for supporting specific annotation types such as Spring's own
@@ -29,11 +31,11 @@ import org.springframework.transaction.interceptor.TransactionAttribute;
  * or EJB3's {@link javax.ejb.TransactionAttribute}.
  *
  * @author Juergen Hoeller
- * @since 2.5
  * @see AnnotationTransactionAttributeSource
  * @see SpringTransactionAnnotationParser
  * @see Ejb3TransactionAnnotationParser
  * @see JtaTransactionAnnotationParser
+ * @since 2.5
  */
 public interface TransactionAnnotationParser {
 
@@ -42,6 +44,7 @@ public interface TransactionAnnotationParser {
 	 * based on an annotation type understood by this parser.
 	 * <p>This essentially parses a known transaction annotation into Spring's metadata
 	 * attribute class. Returns {@code null} if the method/class is not transactional.
+	 *
 	 * @param element the annotated method or class
 	 * @return the configured transaction attribute, or {@code null} if none found
 	 * @see AnnotationTransactionAttributeSource#determineTransactionAttribute
